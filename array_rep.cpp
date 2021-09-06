37f1b718a80cd4e213fe25d98a1149df9ed22af4
Abhishek Bisht <abhishekbisht1429@gmail.com>
2021-09-06 17:12:07 +0530

    implemented Addition of matrices

---
+++ array_rep.cpp
@@ -0,0 +1,178 @@
+#include<iostream>
+#include<fstream>
+#include<chrono>
+
+#define DEBUG
+using namespace std;
+
+struct node {
+    int r, c;
+    double val;
+
+    int comp(const node &nd) {
+        if(this->r < nd.r) {
+            return -1;
+        } else if(this->r == nd.r) {
+            if(this->c < nd.c)
+                return -1;
+            else if(this->c == nd.c)
+                return 0;
+            else 
+                return 1;
+        } else {
+            return 1;
+        }
+    }
+
+    bool operator<(const node &nd) {
+        return comp(nd) < 0;
+    }
+
+    bool operator>(const node &nd) {
+        return comp(nd) > 0;
+    }
+
+    bool operator==(const node &nd) {
+        return comp(nd) == 0;
+    }
+};
+
+class sparse_matrix_2d {
+    node *arr;
+    int len;
+    int r, c;
+
+    public:sparse_matrix_2d(int n, int r, int c) {
+        len = n;
+        arr = new node[n];
+        this->r = r;
+        this->c = c;
+    }
+    public:sparse_matrix_2d(double **mat, int r, int c) {
+        this->r = r;
+        this->c = c;
+        this->len = 0;
+        for(int i=0; i<r; ++i)
+            for(int j=0; j<c; ++j)
+                if(mat[i][j] != 0) len++;
+        
+        arr = new node[len];
+
+        int k = 0;
+        for(int i=0; i<r; ++i) {
+            for(int j=0; j<c; ++j) {
+                if(mat[i][j] != 0) {
+                    arr[k].r = i;
+                    arr[k].c = j;
+                    arr[k].val = mat[i][j];
+                    k++;
+                }
+            }
+        }
+    }
+
+    public:sparse_matrix_2d(const sparse_matrix_2d &mat) {
+        *this = mat;
+    }
+
+    public:void operator=(const sparse_matrix_2d &mat) {
+        this->len = mat.len;
+        this->r = mat.r;
+        this->c = mat.c;
+        this->arr = new node[this->len];
+        for(int i=0; i<len; ++i)
+            arr[i] = mat.arr[i];
+    }
+
+    ~sparse_matrix_2d() {
+        delete[] arr;
+    }
+
+    public:sparse_matrix_2d operator+(const sparse_matrix_2d &mat) {
+        if(this->r != mat.r && this->c != mat.c)
+            return sparse_matrix_2d(0, 0, 0);
+
+        int i=0, j=0;
+        int max_len = this->len + mat.len;
+        int k = 0;
+        sparse_matrix_2d sum(max_len, this->r, this->c);
+        while(i<this->len && j<mat.len) {
+            if(arr[i] == mat.arr[j]) {
+                sum.arr[k].r = arr[i].r;
+                sum.arr[k].c = arr[i].c;
+                sum.arr[k].val = arr[i].val + mat.arr[j].val;
+                i++;
+                j++;
+            } else if(arr[i] < mat.arr[j]) {
+                sum.arr[k].r = arr[i].r;
+                sum.arr[k].c = arr[i].c;
+                sum.arr[k].val = arr[i].val;
+                i++;
+            } else {
+                sum.arr[k].r = mat.arr[j].r;
+                sum.arr[k].c = mat.arr[j].c;
+                sum.arr[k].val = mat.arr[j].val;
+                j++;
+            }
+            k++;
+        }
+        sum.len = k;
+
+        return sum;
+    }
+
+    public:void display() {
+        for(int i=0; i<len; ++i)
+            cout<<arr[i].r<<" "<<arr[i].c<<" "<<arr[i].val<<"\n";
+        cout<<"\n";
+    }
+
+};
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    #ifdef DEBUG
+    ifstream _in("input.txt");
+    ofstream _out("output.txt");
+    streambuf* _in_backup = cin.rdbuf();
+    streambuf* _out_backup = cout.rdbuf();
+    cin.rdbuf(_in.rdbuf());
+    cout.rdbuf(_out.rdbuf());
+    auto _start = chrono::high_resolution_clock::now();
+    #endif
+    /* ######################CODE_START################################ */
+    int n, m;
+    cin>>n>>m;
+    double **arr = new double*[n];
+    for(int i=0; i<n; ++i)
+        arr[i] = new double[m];
+
+    for(int i=0; i<n; ++i)
+        for(int j=0; j<m; ++j)
+            cin>>arr[i][j];
+    sparse_matrix_2d mat1(arr, n, m);
+
+    for(int i=0; i<n; ++i)
+        for(int j=0; j<m; ++j)
+            cin>>arr[i][j];
+    sparse_matrix_2d mat2(arr, n, m);
+
+    mat1.display();
+    mat2.display();
+
+    sparse_matrix_2d sum = mat1 + mat2;
+    sum.display();
+
+    /* #######################CODE_END############################### */
+    #ifdef DEBUG
+    auto _end = chrono::high_resolution_clock::now();
+    auto _duration = chrono::duration_cast<chrono::nanoseconds>(_end - _start);
+    long long _ns = _duration.count();
+    double _s = _ns / 1e9;
+    cout<<"\nDuration: "<<_s<<" sec\n";
+    cin.rdbuf(_in_backup);
+    cout.rdbuf(_out_backup);
+    #endif
+    return 0;
+}
